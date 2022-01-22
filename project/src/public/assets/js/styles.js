7b7c49ce5a540e9834594f32ec5bf64d571b13c4
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-01-22 21:36:21 +0000

    work on parallex scroll for header animation

--- project/src/public/assets/js/styles.js
+++ project/src/public/assets/js/styles.js
@@ -1,7 +1,33 @@
 // separation of concerns
 // non-functional logic for styling for animation of hero image- parallex scroll, etc.
 
-const regex = /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/,
-const landscape = document.getElementById('landscape');
-const landscapeLayers = document.querySelectorAll('path');
-const sun = document.getElementById('sun');
+
+
+var regex = /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/,
+  landscape = document.getElementById("landscape"),
+  landscapeLayers = landscape.querySelectorAll("g"),
+  // SVGoffsettop = landscape.getBoundingClientRect().top,
+  vertHeight = landscape.getBoundingClientRect().height,
+  sun = document.getElementById("sun");
+
+function scrollHandler() {
+  if (window.scrollY < vertHeight) {
+    Array.prototype.forEach.call(landscapeLayers, function(layer) {
+      const layerFill = layer.getAttribute("fill"),
+        vertRoll = Math.abs(window.scrollY - vertHeight) / vertHeight;
+      hslComponents = layerFill.match(regex).slice(1),
+        newHSL = parseFloat(hslComponents[2]) * vertRoll;
+      layer.style.fill = "hsl(" + hslComponents[0] + ", " + hslComponents[1] + "%, " + newHSL + "%)";
+      landscape.style.background = "hsl(202, " + 100 * vertRoll + "%, " + 88 * vertRoll + "%)";
+      sun.style.transform = "translate3d(0," + window.scrollY / 2 + "px, 0)";
+    })
+  } else {
+    landscape.style.transform = "translateY(-" + (window.scrollY - vertHeight) + "px)";
+  }
+}
+
+window.onscroll = function() {
+  window.requestAnimationFrame(scrollHandler);
+}
+
+
