82aae6a78ef62ec17fd76856443581adb500bf53
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-01-23 23:04:59 +0000

    minor tweaks to fetch functions- images and roverinfo

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -20,71 +20,6 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 const apiKey = process.env.API_KEY;
 
 
-// const Rovers = Immutable.List([Curiosity, Spirit, Opportunity]);
-
-// const Rovers2 = Rovers.set([1, Perserverance ]);
-
-
-
-// app.get('/roverImages', async (req, res) => {
-//     try {
-//         let image = await fetch(
-//             `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`
-//         ).then((res) => res.json());
-//         res.send({ image });
-//     } catch (err) {
-//         console.log('error:', err);
-//     }
-// });
-// https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${apiKey}
-
-// example API call
-// If you just want to receive photo data for the most recent Sol for which photos exist for a particular rover, you can visit the /latest_photos endpoint.
-
-// `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${apiKey}`
-// https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos
-// https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos
-
-// ** for data
-
-// https://api.nasa.gov/mars-photos/api/v1/manifests/<Rover Name>/
-// landing_date
-// launch_date
-// status
-// max_sol
-// max_date
-// https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=YOUR-API-KEY
-
-
-// more info here https://github.com/chrisccerami/mars-photo-api#query-for-latest-photos
-
-
-// const latestImages = (rover, earthDate index) => { 
-//app.get(/${rover}/latest_photos, async (req, res) => try { let latestImages = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=apiKey`.then((res) => res.json()).res.send)({image})} catch (error) {console.log('error:', err);})
-//})
-
-// const roverImages = (rover, index) => { 
-//app.get(/${rover}/latest_photos, async (req, res) => try { let latestImages = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=apiKey`.then((res) => res.json()).res.send)({image})} catch (error) {console.log('error:', err);})
-//})
-//
-//
-//}
-
-
-//When the user clicks on the rovers name the following info change according to the clicked rover:
-// - Revor Name.
-
-// - Launch Date.
-
-// - Landing Date.
-
-// - Status.
-
-// - Date Of Most Recent Photos Were Taken.
-
-// - Most Recently Available Photos.
-
-
 // Fetching rover photos from the Mars api using dynamic router
 
 
@@ -92,8 +27,8 @@ app.get('/rovers/:rover/', async (req, res) => {
     console.log(req.params);
     console.log(req.query);
     try {
-     let roverImages = await fetch (`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.rover}/latest_photos?api_key=${apiKey}`)
-                .then((res) => res.json()); res.send({roverImages});
+     let roverImage = await fetch (`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.rover}/latest_photos?api_key=${apiKey}`)
+                .then((res) => res.json()); res.send({roverImage});
             
         } catch (error) {console.log('error:', error);
         }
@@ -106,7 +41,7 @@ app.get('/rovers/:rover/', async (req, res) => {
 app.get('manifests/:chosenRover', async (req, res) => {
     const rover = req.params.chosenRover ? req.params.chosenRover : 'Curiosity';
     try {
-        const roverInfo = await fetch (`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${apiKey}`)
+        let roverInfo = await fetch (`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${apiKey}`)
             .then((res) => res.json()); res.send({roverInfo});
                 
     } catch (error) {console.log('error:', error); }
